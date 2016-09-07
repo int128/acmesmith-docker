23c86ed54ce4f3c8349d7d4d74fe9e2606cb4035
Hidetake Iwata <int128@gmail.com>
2016-09-07 15:36:34 +0900

    Use full chain certificate for nginx

--- renew.sh
+++ renew.sh
@@ -12,7 +12,7 @@ cd "`dirname $0`"
 mkdir -p "$WEB_SERVICE/cert"
 docker-compose build acmesmith
 docker-compose run --rm acmesmith autorenew
-docker-compose run --rm acmesmith show-certificate "$COMMON_NAME" > "$WEB_SERVICE/cert/cert.pem"
+docker-compose run --rm acmesmith show-certificate "$COMMON_NAME" --type=fullchain > "$WEB_SERVICE/cert/cert.pem"
 docker-compose run --rm acmesmith show-private-key "$COMMON_NAME" > "$WEB_SERVICE/cert/key.pem"
 docker-compose build "$WEB_SERVICE"
 docker-compose up -d "$WEB_SERVICE"
